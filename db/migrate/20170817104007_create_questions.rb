4f2ddb5b6aa828c80cd129fd16652b066663ef7c
Dillon Forestell & George Dzuricsko & Ida Lu & Joseph Connors <pair+dforestell.geoclimber4.idaylu.jcce@devbootcamp.com>
2017-08-17 10:46:13 -0500

    update migratation tables

--- db/migrate/20170817104007_create_questions.rb
+++ db/migrate/20170817104007_create_questions.rb
@@ -1,4 +1,12 @@
 class CreateQuestions < ActiveRecord::Migration
   def change
+    create_table :questions do |t|
+      t.string    :title
+      t.string    :text
+      t.integer   :author_id
+      t.integer   :best_answer_id
+
+      t.timestamps
+    end
   end
 end
--- db/migrate/20170817104023_create_comments.rb
+++ db/migrate/20170817104023_create_comments.rb
@@ -1,4 +1,12 @@
 class CreateComments < ActiveRecord::Migration
   def change
+    create_table :comments do |t|
+      t.integer   :author_id
+      t.string    :text
+      t.integer   :commentable_id
+      t.string    :commentable_type
+
+      t.timestamps
+    end
   end
 end
