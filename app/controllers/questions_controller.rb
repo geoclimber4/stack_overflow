8912324b7e827d660a16eea00b437dbffcc84949
Dillon Forestell & George Dzuricsko & Ida Lu & Joseph Connors <pair+dforestell.geoclimber4.idaylu.jcce@devbootcamp.com>
2017-08-17 15:40:45 -0500

    add controller and views for question/edit

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -27,8 +27,29 @@ post '/questions' do
   end
 end
 
+get  '/questions/:id/edit' do
+  @question = Question.find(params[:id])
+  erb :'questions/edit'
+end
+
+put '/questions/:id' do
+  @question = Question.find(params[:id])
+  p params
+  p params[:question]
+  @question.assign_attributes(params[:question])
+  if @question.save
+    redirect '/questions'
+  else
+    @errors = @question.errors.full_messages
+    erb ':questions/edit'
+  end
+end
+
 delete '/questions/:id' do
   @question = Question.find(params[:id])
   @question.destroy
+  # eventually delete all supporting answers, comments, votes
   redirect '/questions'
 end
+
+
